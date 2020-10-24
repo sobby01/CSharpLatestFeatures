9c9e5c3e2d13ed53f890d0d3273906862ce58ff0
Saurabh Singh <saurabh.singh@winshuttle.com>
2020-10-24 06:23:02 +0530

    Tuples, Async Methods and Dynamic Binding

--- CSharpLatestFeatures/DynamicBinding.cs
+++ CSharpLatestFeatures/DynamicBinding.cs
@@ -6,6 +6,23 @@ namespace CSharpLatestFeatures
 {
     public class DynamicBinding
     {
+        public dynamic Name { get; set; }
+
+        public dynamic ShowMsg(string msg)
+        {
+            return msg;
+        }
+
+        public void DynamicImpl()
+        {
+            this.Name = "Saurabh Singh";
+
+            Console.WriteLine(this.Name);
+
+            dynamic msg = ShowMsg("This is Saurabh");
+            Console.Write(msg);
+        }
+
         public void DynamicMethod()
         {
             dynamic v = 1;
